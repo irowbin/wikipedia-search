0ae9dc80386bf9891681e424f720c0d7159a8f00
Robin <irowbin@gmail.com>
2024-11-14 15:42:18 +0545

    fix: move inline variable to computed property to avoid view expression error
    style: add SVG content for look and feel

--- src/app/components/typeahead-container/typeahead-container.component.ts
+++ src/app/components/typeahead-container/typeahead-container.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core'
+import { Component, computed, ElementRef, ViewChild } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { TypeaheadControlComponent } from '../typeahead-control/typeahead-control.component'
+import { TypeaheadSelectedComponent } from '@/components/typeahead-selected/typeahead-selected.component'
 
 @Component({
   selector: 'app-typeahead-container',
   standalone: true,
-  imports: [CommonModule, TypeaheadControlComponent],
+  imports: [
+    CommonModule,
+    TypeaheadControlComponent,
+    TypeaheadSelectedComponent,
+  ],
   templateUrl: './typeahead-container.component.html',
   styleUrl: './typeahead-container.component.scss',
 })
-export class TypeaheadContainerComponent {}
+export class TypeaheadContainerComponent {
+  @ViewChild('container')
+  public container: ElementRef<HTMLDivElement>
+
+  /**
+   * The width of the container element in pixels.
+   *
+   * Used for selected typeahead item container width calculation.
+   * */
+  public readonly containerWidth = computed(
+    () => this.container?.nativeElement?.clientWidth
+  )
+}
