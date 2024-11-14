5f8f4859ee72511514f65dad64a715d8e7484b0b
Robin <irowbin@gmail.com>
2024-11-14 15:37:57 +0545

    refactor: move the selected typeahead widget to another place

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,56 +1,17 @@
-import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core'
-import { RouterModule } from '@angular/router'
-import { TypeaheadControlComponent } from './components/typeahead-control/typeahead-control.component'
+import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { TypeaheadUserManualComponent } from './components/typeahead-user-manual/typeahead-user-manual.component'
-import { TypeaheadSelectedComponent } from './components/typeahead-selected/typeahead-selected.component'
 import { TypeaheadContainerComponent } from './components/typeahead-container/typeahead-container.component'
-import { Subject, takeUntil } from 'rxjs'
-import { SearchResult } from './models/typeahead.model'
-import { TypeaheadStateFacade } from './+state/typeahead-state.facade'
 
 @Component({
   standalone: true,
   imports: [
-    RouterModule,
     CommonModule,
-    TypeaheadControlComponent,
     TypeaheadUserManualComponent,
-    TypeaheadSelectedComponent,
     TypeaheadContainerComponent,
   ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnDestroy, OnInit {
-  /** Manages subscriptions when the component is destroyed */
-  private readonly toDestroy$ = new Subject<void>()
-
-  /** Selected typeahead object signal with undefined initially */
-  public readonly selectedTypeahead = signal<SearchResult>(undefined)
-
-  /** Injects the TypeaheadStateFacade for state management */
-  private readonly typeaheadFacade = inject(TypeaheadStateFacade)
-
-  public ngOnInit(): void {
-    this.#selectSelectedTypeahead()
-  }
-
-  public ngOnDestroy(): void {
-    this.toDestroy$.next()
-    this.toDestroy$.complete()
-  }
-
-  /**
-   * Selects the selected typeahead from the state.
-   * @returns {void}
-   */
-  #selectSelectedTypeahead(): void {
-    this.typeaheadFacade.selectSelectedTypeahead$
-      .pipe(takeUntil(this.toDestroy$))
-      .subscribe((typeahead) => {
-        this.selectedTypeahead.set(typeahead)
-      })
-  }
-}
+export class AppComponent {}
